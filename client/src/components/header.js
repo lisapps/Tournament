daea04f330ddd984080b21dd3c2d4a0b6b86f6d6
Lisa Ziegler <lisa_ziegler@kingston.com>
2020-04-14 11:03:57 -0700

    added header component and cleaned up package json

---
+++ client/src/components/header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <section className="c-header">
+      <div className=".c-header__right">
+        <div className="c-account">
+          <div className="c-login">
+            <span>Hi, User</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
